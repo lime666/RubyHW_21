a3f33c65c83737e638f05b023a4d7ab1cd890e96
Yulia Lime <lime666lime@gmail.com>
2021-11-29 20:27:54 +0200

    bugs fixed & rubocop

--- HW2/pet/animal.rb
+++ HW2/pet/animal.rb
@@ -1,142 +1,158 @@
 require 'time'
 
 class Animal
-  attr_accessor :name
 
-def initialize name
-  @name = name
-  @asleep = false
-  @stuffInBelly = 10
-  @stuffInIntestine = 0
-  @lives = 10
-  @energy = 10
-  @birthTime = Time.new
-
-end
-
-def eat
-  if @stuffInBelly <= 5 && @energy <= 5
-    sleep 2
-    puts "Спасибо, было очень вкусно, хотя я хз чем ты меня накормил."
-    puts "Теперь хочу тусить."
-    @energy += 2
-    @stuffInBelly += 3
-    @stuffInIntestine += 2
-  else 
-    puts "Спасибо, я не голоден."
-  end
-end
-
-def rest
-  puts "Вы укладываете животину спать."
-  @asleep = true
-  @energy += 3
-  time_to_pass
-end
-
-def rave
-  puts "Вы играете с #{@name}."
-  @energy -= 2
-  if @energy <= 6
-    puts "Жизнь меня к такому не готовила, хозяин."
-  end
-  time_to_pass
-end
-
-def wakeup
-  @asleep = false
-  p "За что, хозяин?"
-  p "Ну, раз ты меня рабудил..Чем займемя, милашка?"
-end
-
-def pet
-  puts "Вы чешите #{@name} пузико."
-  @energy -= 1
-  sleep 2
-  p "Спасибушки, хозяин. Я бы тоже хотел тебя почесать =)"
-  time_to_pass
-end
-
-def bath
-  puts "#{@name} купется."
-  @energy -= 1
-  time_to_pass
-end
-
-
-def wc
-  if @stuffInIntestine >= 7
-    puts "Жизнь обрела краски."
-    @stuffInIntestine = 0
-  else
-    puts "Чуть позже, пожалуйста."
-  end
-end
-
-def voice
-  p "Во время линьки у некоторых птиц утрачивается способность к полету. "\
-  "Так, например, утки при этом не могут летать 20-35 суток, "\
-  "лебеди – почти 1,5 месяца."
-end
-
-def help
-  puts "eat - накормить"
-  puts "rest - положить спать"
-  puts "rave - поиграть"
-  puts "repeat - повторить действие"
-  puts "wakeup - разбудить"
-  puts "pet - учить"
-  puts "bath - купаться"
-  puts "voice - животина что-то поведает"
-  puts "wc - опорожнить кишечник"
-  puts "help - помощь"
-  puts "exit - выйти из игры"
-end
-
-def age
-  pet_age
-end
-
-def repeat
-#  command. { |i| 1.times do i end }
-end
-
-private
-
-def hungry?
-  @stuffInBelly <= 3
-  puts "Зачем я живу? Даже покормить некому..."
-end
-
-def time_to_pass
-  if @stuffInBelly > 0
-    @stuffInBelly -= 1
-    @stuffInIntestine +=1
-  elsif @asleep
+  def initialize(name)
+    @name = name
     @asleep = false
-    @stuffInBelly -= 1
+    @stuff_in_belly = 10
+    @stuff_in_intestine = 0
+    @lives = 10
+    @energy = 10
+    @birth_time = Time.new
   end
-end
 
-def want_to_sleep
-  if (@asleep == false) & (@energy <= 2)
-    puts "Где упал, там и кровать."
+  def eat
+    if @stuff_in_belly <= 5 && @energy <= 5
+      sleep 2
+      p 'Спасибо, было очень вкусно, хотя я хз чем ты меня накормил.'
+      p 'Теперь хочу тусить.'
+      @energy += 2
+      @stuff_in_belly += 3
+      @stuff_in_intestine += 2
+    else
+      p 'Спасибо, я не голоден.'
+    end
   end
-end
 
-def death
-  if (@stuffInBelly == 0) || (@energy == 0)
-  	@lives -= 1
-  elsif (@stuffInBelly == 2)  || (@energy == 2)
-  	puts "Смерть идет по пятам. Знать бы что нас ждет впереди..."
-  elsif @stuffInBelly > 10 
-    puts "#{@name} уже не с нами. Готовьте пирожки."
+  def rest
+    p 'Вы укладываете животину спать.'
+    @asleep = true
+    @energy += 3
+    time_to_pass
   end
-end
 
-def pet_age
-  age = Time.now - @birthTime
-  p age
-end
+  def rave
+    p "Вы играете с #{@name}."
+    @energy -= 2
+    p 'Жизнь меня к такому не готовила, хозяин.' if @energy <= 6
+    time_to_pass
+    p 'Не любишь ты меня, хозяин. Помер я.' if death
+  end
 
+  def wakeup
+    @asleep = false
+    p 'За что, хозяин?'
+    p 'Ну, раз ты меня рабудил..Чем займемя, милашка?'
+  end
+
+  def pet
+    p "Вы чешите #{@name} пузико."
+    @energy -= 1
+    p 'ур-ур-ур '
+    sleep 2
+    p 'Спасибушки, хозяин. Я бы тоже хотел тебя почесать =)'
+    time_to_pass
+  end
+
+  def bath
+    p "#{@name} купется."
+    @energy -= 1
+    time_to_pass
+  end
+
+  def wc
+    if @stuff_in_intestine >= 7
+      p 'Жизнь обрела краски.'
+      @stuff_in_intestine = 0
+    else
+      p 'Чуть позже, пожалуйста.'
+    end
+  end
+
+  def voice
+    p 'Во время линьки у некоторых птиц утрачивается способность к полету. '\
+      'Так, например, утки при этом не могут летать 20-35 суток, '\
+      'лебеди – почти 1,5 месяца.'
+  end
+
+  def help
+    p 'eat - накормить'
+    p 'rest - положить спать'
+    p 'rave - поиграть'
+    p 'repeat - повторить действие'
+    p 'wakeup - разбудить'
+    p 'pet - учить'
+    p 'bath - купаться'
+    p 'voice - животина что-то поведает'
+    p 'wc - опорожнить кишечник'
+    p 'help - помощь'
+    p 'status - что происходит'
+    p 'exit - выйти из игры'
+  end
+
+  def status
+    p 'Зачем я живу? Даже покормить некому...' if hungry?
+    p 'Где упал, там и кровать.' if want_to_sleep?
+    p '...' if death
+    p "#{@lives} - осталось жизней."
+    p "#{@energy} - уровень бодрости."
+    p "#{@stuff_in_belly} - уровень еды внутри."
+    p "#{@stuff_in_intestine} - решайте сами вести в уборную или нет."
+  end
+
+  def age
+    pet_age
+  end
+
+  private
+
+  def hungry?
+    @stuff_in_belly <= 3
+  end
+
+  def time_to_pass
+    if @stuff_in_belly.positive?
+      @stuff_in_belly -= 1
+      @stuff_in_intestine += 1
+    elsif @asleep
+      @asleep = false
+      @stuff_in_belly -= 1
+    end
+  end
+
+  def want_to_sleep?
+    if @asleep == false && @energy <= 2
+    end
+  end
+
+  def death
+    if @stuff_in_belly.zero? || @energy.zero?
+      @lives -= 1
+      @asleep = false
+      @stuff_in_belly = 10
+      @stuff_in_intestine = 0
+      @energy = 10
+      p "Осталось #{@lives} жизней."
+    elsif @stuff_in_belly == 2 || @energy == 2
+      @lives -= 1
+      @asleep = false
+      @stuff_in_belly = 10
+      @stuff_in_intestine = 0
+      @energy = 10
+      p 'Смерть идет по пятам. Знать бы что нас ждет впереди...'
+    elsif @stuff_in_belly > 10
+      @lives -= 1
+      @asleep = false
+      @stuff_in_belly = 10
+      @stuff_in_intestine = 0
+      @energy = 10
+      p "#{@name} уже не с нами. Готовьте пирожки."
+    end
+  end
+
+  def pet_age
+    age = Time.now - @birth_time
+    p age
+  end
 end
--- HW2/pet/menu_animal.rb
+++ HW2/pet/menu_animal.rb
@@ -3,59 +3,54 @@ require 'time'
 class Menu
   require_relative 'animal'
 
-  COMMANDS = ['eat', 'rest', 'rave', 'wakeup', 'pet', 'bath', 'wc', 'voice', 'help', 'age', 'repeat', 'exit']
+  COMMANDS = %w[eat rest rave wakeup pet bath wc voice help status age exit]
 
-def create_animal
-  puts "Как бы Вы хотели назвать животину?"
-  name = gets.chomp
-	@animal = Animal.new(name)
-  puts "Подождите немного пока животина родится."
-  sleep 5
-  puts "#{name} родился. Назад дороги нет."
-  sleep 2
-end
+  def create_animal
+    p 'Как бы Вы хотели назвать животину?'
+    name = gets.chomp
+    @animal = Animal.new(name)
+    p 'Подождите немного пока животина родится.'
+    sleep 5
+    p "#{name} родился. Назад дороги нет."
+    sleep 2
+  end
 
-def commandd
-  puts "Выберите действие: "
-  puts COMMANDS
-  command = gets.chomp.downcase
-end
-  
-def start_to_play
-  create_animal
-  while true 
-  	command = commandd
-  	case command
-  	  when 'eat'
-  	  	@animal.eat
-  	  when 'rest'
-  	  	@animal.rest
-  	  when 'rave'
-  	  	@animal.rave
-  	  when 'wakeup'
-  	  	@animal.wakeup
-  	  when 'pet'
-  	  	@animal.pet
-  	  when 'bath'
-  	  	@animal.bath
-  	  when 'wc'
-  	  	@animal.wc
-  	  when 'help'
-  	  	@animal.help
-  	  when 'age'
-      	@animal.age
+  def start_to_play
+    create_animal
+    while true
+      p 'Выберите действие: '
+      p COMMANDS
+      command = gets.chomp.downcase
+      case command
+      when 'eat'
+        @animal.eat
+      when 'rest'
+        @animal.rest
+      when 'rave'
+        @animal.rave
+      when 'wakeup'
+        @animal.wakeup
+      when 'pet'
+        @animal.pet
+      when 'bath'
+        @animal.bath
+      when 'wc'
+        @animal.wc
+      when 'help'
+        @animal.help
+      when 'status'
+        @animal.status
+      when 'age'
+        @animal.age
       when 'voice'
         @animal.voice
-      when 'repeat'
-        @animal.repeat
-  	  when 'exit'
-  	  	break
-  	  else
-  	  	puts "Отдохни, хозяин. Ты устал и не понимаешь о чем говоришь."
-  	  end
-  	end
-end
-
+      when 'exit'
+        break
+      else
+        p 'Отдохни, хозяин. Ты устал и не понимаешь о чем говоришь.'
+      end
+    end
+  end
 end
 
 Menu.new.start_to_play
